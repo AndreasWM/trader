bc9d5046f8e818ef5b0165c2328dc0c307c4a1ad
AndreasWM <MoellerIT@gmail.com>
2026-06-15 18:31:55 +0200

    CAPITAL_RESERVE für die Gewinnermittlung abziehen

--- src/ibkr_portfolio_manager.py
+++ src/ibkr_portfolio_manager.py
@@ -150,7 +150,7 @@ class PortfolioManager:
                 wanted = True
                 print(f"Update kann durchgeführt werden, da bereits mehr als {state.max_age_hours} Stunden seit dem letzten Update vergangen sind.")
             else:
-                increase_in_percentage = (new_liquidation - state.net_liquidation_eur) / state.net_liquidation_eur * 100
+                increase_in_percentage = (new_liquidation - state.net_liquidation_eur) / (state.net_liquidation_eur - CAPITAL_RESERVE) * 100
                 print(f"Alter Depotstand: {state.net_liquidation_eur:.2f} €")
                 print(f"Neuer Depotstand: {new_liquidation:.2f} €")
                 print(f"Zuwachs: {increase_in_percentage:.2f} %")
