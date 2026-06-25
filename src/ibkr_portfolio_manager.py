02ce47cd81fe49873e1c5f642ea1557a99b4c27d
AndreasWM <MoellerIT@gmail.com>
2026-06-25 19:43:08 +0200

    Kleine Änderung

--- src/ibkr_portfolio_manager.py
+++ src/ibkr_portfolio_manager.py
@@ -17,7 +17,7 @@ from lib.yfinance_ticker import YfinanceTicker
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ANALYSIS_FILE = BASE_DIR / 'data' / 'Analysis_Test.txt'
+ANALYSIS_FILE = BASE_DIR / 'data' / 'Analysis_Prod.txt'
 CAPITAL_RESERVE = 0
 DIVIDENDS_PERCENT = 3.0
 FLAG_LONG = True
